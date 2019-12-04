2e703796bede1864da33fbd769c87058118fa969
Nick Tholin <Mcfly130@gmail.com>
2019-12-03 23:30:56 -0600

    Add Day1 - Fuel Calculations

---
+++ AoC2019/Day1/SimpleFuelCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AoC2019.Day1
+{
+    public sealed class SimpleFuelCalculator : IFuelCalculator
+    {
+        private IFuelCalculator _additionalCalculator;
+
+        public SimpleFuelCalculator(IFuelCalculator additionalCalculator = null)
+        {
+            _additionalCalculator = additionalCalculator;
+        }
+
+        public int CalculateFuelRequirements(int mass)
+        {
+            var fuelRequired = (int)Math.Floor(mass / 3M) - 2;
+
+            if (_additionalCalculator == null) return fuelRequired;
+
+            return fuelRequired += _additionalCalculator.CalculateFuelRequirements(fuelRequired);
+        }
+    }
+}
