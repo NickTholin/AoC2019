59bc179c062e0e03adccfb925e5fb59662f73847
NickTholin <nicholastholin@gmail.com>
2019-12-08 00:04:26 -0600

    Added Day2

---
+++ AoC2019/Day2/IntCode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC2019.Day2
+{
+    public class IntCode
+    {
+
+        public (List<int> Memory, int Noun, int Verb) FindIntCodeFromGoal(List<int> memory, int goal)
+        {
+            var temporaryMemory = new List<int>(memory);
+            var runResult = temporaryMemory;
+            int noun = 0;
+            int verb = 0;
+
+            for (noun = 0; noun < 100; noun++)
+            {
+                for (verb = 0; verb < 100; verb++)
+                {
+                    temporaryMemory[1] = noun;
+                    temporaryMemory[2] = verb;
+                    runResult = Run(temporaryMemory);
+                    if (runResult[0] == goal) break;
+                }
+                if (runResult[0] == goal) break;
+            }
+
+            return (runResult, noun, verb);
+        }
+
+        public List<int> Run(List<int> memory)
+        {
+            var address = 0;
+            var temporaryMemory = new List<int>(memory);
+
+            while (address < temporaryMemory.Count())
+            {
+                var instruction = temporaryMemory[address];
+
+                //The value at position address + 1, address + 2 denote the location of in memory of the value we actually want to use.
+                var parameter1 = temporaryMemory[temporaryMemory[address + 1]];
+                var parameter2 = temporaryMemory[temporaryMemory[address + 2]];
+
+                int newValue;
+                if (instruction == 1)
+                    newValue = parameter1 + parameter2;
+                else if (instruction == 2)
+                    newValue = parameter1 * parameter2;
+                else
+                    break;
+
+                //Value at address + 3 denotes the position we should store the new value at.
+                temporaryMemory[temporaryMemory[address + 3]] = newValue;
+
+                //Iterating over blocks of 4 numbers at a time.
+                address += 4;
+            }
+
+            return temporaryMemory;
+        }
+    }
+}
--- AoC2019/Program.cs
+++ AoC2019/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using AoC2019.Day1;
+using AoC2019.Day2;
 
 namespace AoC2019
 {
@@ -7,11 +9,35 @@ namespace AoC2019
     {
         static void Main(string[] args)
         {
-            //Day1
+            //Day1 Part 2
             var fuelCalculator = new RecursiveFuelCalculator(new SimpleFuelCalculator());
             var componentList = new ComponentList(@"Day1/ComponentMasses.txt", fuelCalculator);
             Console.WriteLine($"Required Fuel: {componentList.FuelRequired}");
             Console.ReadKey();
+
+            //Day2
+            var intCodeSequence = System.IO.File.ReadAllText("Day2/IntCodes.txt");
+            var intCodes = intCodeSequence
+                .Split(",")
+                .Select(int.Parse)
+                .ToList();
+            var intCode = new IntCode();
+            var memory = intCode.Run(intCodes);
+
+            Console.WriteLine(memory[0]);
+            Console.ReadKey();
+
+            //Part2
+            var intCodeGoal = intCode.FindIntCodeFromGoal(intCodes, 19690720);
+            Console.WriteLine();
+            Console.WriteLine($"IntCode[0]: {intCodeGoal.Memory[0]} Noun: {intCodeGoal.Noun} Verb: {intCodeGoal.Verb}");
+            Console.ReadKey();
+
+        }
+
+        public static void log(string s)
+        {
+            Console.WriteLine(s);
         }
     }
 }
