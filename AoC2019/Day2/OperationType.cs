568d2d0a4c667928342b09984ea28e7e4fe717ff
NickTholin <nicholastholin@gmail.com>
2019-12-08 00:04:26 -0600

    Added Day2

---
+++ AoC2019/Day2/OperationType.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC2019.Day2
+{
+    //1,2,99 as defined in instructions
+    public enum OperationType
+    {
+        Addition = 1,
+        Multiplication = 2,
+        Fault = 99
+    }
+}
